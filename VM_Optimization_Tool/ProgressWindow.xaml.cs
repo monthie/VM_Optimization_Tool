ebf25e43b3c7c9070a3d643ee1b10944f2ac2d1d
monthie <42239824+monthie@users.noreply.github.com>
2019-01-25 00:47:19 +0100

    win updates

--- VM_Optimization_Tool/ProgressWindow.xaml.cs
+++ VM_Optimization_Tool/ProgressWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 //using System.Threading.Tasks;
@@ -11,17 +12,35 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WUApiLib;
 
 namespace VM_Optimization_Tool
-{
+{  
     /// <summary>
     /// Interaktionslogik für ProgressWindow.xaml
     /// </summary>
     public partial class ProgressWindow : Window
     {
+
+        public BackgroundWorker bgWorker;
+        
+
         public ProgressWindow()
         {
             InitializeComponent();
+            bgWorker = new BackgroundWorker();
+            bgWorker.WorkerSupportsCancellation = true;
+            bgWorker.WorkerReportsProgress = true;
+        }
+
+        public void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- VM_Optimization_Tool/WindowsUpdateFrame.xaml.cs
+++ VM_Optimization_Tool/WindowsUpdateFrame.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using WUApiLib;
 
 namespace VM_Optimization_Tool
@@ -20,26 +21,35 @@ namespace VM_Optimization_Tool
     /// </summary>
     public partial class WindowsUpdateFrame : Window
     {
+        private UpdateSession uSession;
+        public IUpdateSearcher uSearcher;
+        public ISearchResult sResult;
+        public ProgressWindow progressWindow1;
+        public ProgressWindow progressWindow2;
+        public IInstallationResult installationResult;
+        public IUpdateInstaller installer;
+        public UpdateCollection installCollection;
+        // Asynch Jobs
+        public IDownloadJob downloadJob;
+        public ISearchJob searchJob;
+        public IInstallationJob installationJob;
+
+        public UpdateDownloader updateDownloader;
         public WindowsUpdateFrame()
         {
             InitializeComponent();
-
+            InstallButton.IsEnabled = false;
             CheckForUpdates();
         }
 
         public void CheckForUpdates()
         {
-            UpdateSession uSession = new UpdateSession();
-            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+            uSession = new UpdateSession();
+            uSearcher = uSession.CreateUpdateSearcher();
             uSearcher.Online = true;
             try
             {
-                ISearchResult sResult = uSearcher.Search("IsInstalled=1 And IsHidden=0");
-                textBox1.Text = "Found " + sResult.Updates.Count + " updates" + Environment.NewLine;
-                foreach (IUpdate update in sResult.Updates)
-                {
-                    textBox1.AppendText(update.Title + Environment.NewLine);
-                }
+                searchJob = uSearcher.BeginSearch("IsInstalled=0 And IsHidden=0", new SearchCompletedFunc(this), null);
             }
             catch (Exception ex)
             {
@@ -47,15 +57,135 @@ namespace VM_Optimization_Tool
             }
         }
 
+        public void DownloadUpdates(UpdateCollection uCollection)
+        {
+            updateDownloader = new UpdateDownloader();
+            progressWindow1 = new ProgressWindow();
+            progressWindow1.Show();
+            progressWindow1.Title = "Downloading...";
+            downloadJob = null;
+            updateDownloader.Updates = uCollection;
+            downloadJob = updateDownloader.BeginDownload(new DownloadProgressChangedFunc(this), new DownloadCompletedFunc(this), null);
+            BeginInstallation();
+        }
+
+
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        {     
+            DownloadUpdates(sResult.Updates);
+        }
+
+        private void BeginInstallation()
         {
-            ProgressWindow progressWindow = new ProgressWindow();
-            progressWindow.Show();
+            progressWindow2 = new ProgressWindow();
+            progressWindow2.Show();
+            progressWindow2.Title = "Installation...";
+            installationJob = null;
+            
+            installCollection = new UpdateCollection();
+            foreach (IUpdate update in this.sResult.Updates)
+            {
+                if (update.IsDownloaded)
+                    installCollection.Add(update);
+            }
+            installer = uSession.CreateUpdateInstaller();
+            installer.Updates = installCollection;
+            installationJob = installer.BeginInstall(new InstallProgressChangedFunc(this), new InstallCompletedFunc(this), null);
+        }
+    }
+
+    public class DownloadProgressChangedFunc : IDownloadProgressChangedCallback{
+
+        WindowsUpdateFrame windowsUpdateFrame;
+
+        public DownloadProgressChangedFunc(WindowsUpdateFrame windowsUpdateFrame)
+        {
+            this.windowsUpdateFrame = windowsUpdateFrame;
+        }
+        public void Invoke(IDownloadJob downloadJob, IDownloadProgressChangedCallbackArgs e)
+        {
+            windowsUpdateFrame.progressWindow1.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.progressWindow1.progressBar1.Value = e.Progress.PercentComplete));
+            windowsUpdateFrame.progressWindow1.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.progressWindow1.textBlock1.Text = e.Progress.PercentComplete.ToString()));
+        }
+    }
+
+    public class DownloadCompletedFunc : IDownloadCompletedCallback
+    {
+        WindowsUpdateFrame windowsUpdateFrame;
+        public DownloadCompletedFunc(WindowsUpdateFrame windowsUpdateFrame)
+        {
+            this.windowsUpdateFrame = windowsUpdateFrame;
+        }
+        public void Invoke(IDownloadJob downloadJob, IDownloadCompletedCallbackArgs callbackArgs)
+        {
+            windowsUpdateFrame.progressWindow1.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.progressWindow1.Close()));
+            windowsUpdateFrame.updateDownloader.EndDownload(windowsUpdateFrame.downloadJob);
+            windowsUpdateFrame.downloadJob = null;
+        }
+    }
+    public class SearchCompletedFunc : ISearchCompletedCallback
+    {
+        WindowsUpdateFrame windowsUpdateFrame;
+        public SearchCompletedFunc(WindowsUpdateFrame windowsUpdateFrame)
+        {
+            this.windowsUpdateFrame = windowsUpdateFrame;
+        }
+        public void Invoke(ISearchJob searchJob, ISearchCompletedCallbackArgs callbackArgs)
+        {
+            windowsUpdateFrame.sResult = windowsUpdateFrame.uSearcher.EndSearch(windowsUpdateFrame.searchJob);
+            windowsUpdateFrame.Dispatcher.BeginInvoke(new Action (() => windowsUpdateFrame.textBox1.Text = "Found " + windowsUpdateFrame.sResult.Updates.Count + " updates" + Environment.NewLine));
+            foreach (IUpdate update in windowsUpdateFrame.sResult.Updates)
+            {
+                windowsUpdateFrame.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.textBox1.AppendText(update.Title + Environment.NewLine)));
+            }
+            windowsUpdateFrame.downloadJob = null;
+            if (windowsUpdateFrame.sResult.Updates.Count != 0)
+            {
+                windowsUpdateFrame.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.InstallButton.IsEnabled = true));
+            }
+        }
+    }
+    public class InstallProgressChangedFunc : IInstallationProgressChangedCallback
+    {
+        WindowsUpdateFrame windowsUpdateFrame;
+        public InstallProgressChangedFunc(WindowsUpdateFrame windowsUpdateFrame)
+        {
+            this.windowsUpdateFrame = windowsUpdateFrame;
+        }
+        public void Invoke(IInstallationJob installationJob, IInstallationProgressChangedCallbackArgs e)
+        {
+            windowsUpdateFrame.progressWindow2.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.progressWindow2.progressBar1.Value = e.Progress.PercentComplete));
+            windowsUpdateFrame.progressWindow2.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.progressWindow2.textBlock1.Text = e.Progress.PercentComplete.ToString()));
+        }
+    }
+    public class InstallCompletedFunc : IInstallationCompletedCallback
+    {
+        WindowsUpdateFrame windowsUpdateFrame;
+        public InstallCompletedFunc(WindowsUpdateFrame windowsUpdateFrame)
+        {
+            this.windowsUpdateFrame = windowsUpdateFrame;
+        }
+        public void Invoke(IInstallationJob installationJob, IInstallationCompletedCallbackArgs callbackArgs)
+        {
+            windowsUpdateFrame.installationResult = windowsUpdateFrame.installer.EndInstall(windowsUpdateFrame.installationJob);
+            windowsUpdateFrame.progressWindow2.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.textBox1.Text = "" ));
+            for (int i = 0; i < windowsUpdateFrame.installCollection.Count; i++)
+            {
+                if (windowsUpdateFrame.installationResult.GetUpdateResult(i).HResult == 0)
+                {
+                    windowsUpdateFrame.progressWindow2.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.textBox1.AppendText("Installed : " + windowsUpdateFrame.installCollection[i].Title)));
+                }
+                else
+                {
+                    windowsUpdateFrame.progressWindow2.Dispatcher.BeginInvoke(new Action(() => windowsUpdateFrame.textBox1.AppendText("Failed : " + windowsUpdateFrame.installCollection[i].Title)));
+                }
+            }
+            windowsUpdateFrame.installationJob = null;
         }
     }
 }
